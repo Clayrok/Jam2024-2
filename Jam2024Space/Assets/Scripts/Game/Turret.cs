8936645b4a2885f514b46c376e442850d064eaa4
CLAY\Clay <contact.chaumin@gmail.com>
2024-02-09 22:17:11 -0500

    Added Gizmos circle to show turret auto aim distance in editor

--- Jam2024Space/Assets/Scripts/Game/Turret.cs
+++ Jam2024Space/Assets/Scripts/Game/Turret.cs
@@ -10,6 +10,9 @@ public class Turret : MonoBehaviour
     [SerializeField]
     private float m_ShootDelay = 3f;
 
+    [SerializeField]
+    private float m_AutoAimRange = 5f;
+
     private float m_LastShootTime = float.NegativeInfinity;
 
 
@@ -30,5 +33,36 @@ public class Turret : MonoBehaviour
     private void Shoot()
     {
         Debug.Log("Shoot!");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        DrawGizmosCircle(transform.position, m_AutoAimRange, 32, Color.red);
+    }
+
+    private void DrawGizmosCircle(Vector3 _Position, float _Radius, int _SegmentsCount, Color _Color)
+    {
+        Gizmos.color = _Color;
+
+        float theta = 0f;
+        float deltaTheta = 2f * Mathf.PI / _SegmentsCount;
+        Vector3 prevPoint = Vector3.zero;
+
+        for (int i = 0; i <= _SegmentsCount; i++)
+        {
+            float x = _Radius * Mathf.Cos(theta);
+            float z = _Radius * Mathf.Sin(theta);
+
+            Vector3 currentPoint = _Position + new Vector3(x, 0f, z);
+
+            if (i > 0)
+            {
+                Gizmos.DrawLine(prevPoint, currentPoint);
+            }
+
+            prevPoint = currentPoint;
+            theta += deltaTheta;
+        }
     }
 }
